791b1b395b6f050bb2a853100ef1b6365982cdcf
Jayanth Othayoth <ojayanth@gmail.com>
2026-08-02 04:22:06 -0500

    logging:plugin add registry support
    Add a registry used to associate plugin types with plugin factories.
    
    The registry provides a common mechanism for registering and querying
    plugin implementations. Future plugin management infrastructure will
    use the registry to locate factories associated with plugin
    descriptors.
    
    Add unit tests covering plugin factory lookup.
    
    No functional changes are intended.
    
    Change-Id: Icddd78a906ef7f9aed1891b824d0ef9dbe456b4c
    Signed-off-by: Jayanth Othayoth <ojayanth@gmail.com>

---
+++ plugin/plugin_registry.cpp
@@ -0,0 +1,26 @@
+#include "plugin/plugin_registry.hpp"
+
+#include <utility>
+
+namespace phosphor::logging
+{
+
+void PluginRegistry::registerPlugin(plugin::Type type,
+                                    std::unique_ptr<PluginFactory> factory)
+{
+    registry.emplace(type, std::move(factory));
+}
+
+const PluginFactory* PluginRegistry::lookup(plugin::Type type) const
+{
+    auto it = registry.find(type);
+
+    if (it == registry.end())
+    {
+        return nullptr;
+    }
+
+    return it->second.get();
+}
+
+} // namespace phosphor::logging
