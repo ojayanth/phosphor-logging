00a8132493f1875075f9696a331d6f30efe5b424
Jayanth Othayoth <ojayanth@gmail.com>
2026-08-02 04:22:06 -0500

    logging:plugin add registry support
    Add a registry used to associate plugin types with plugin factories.
    
    The registry provides a common mechanism for registering and querying
    plugin implementations. Future plugin management infrastructure will
    use the registry to locate factories associated with plugin
    descriptors.
    
    Add unit tests covering plugin factory lookup.
    
    No functional changes are intended.
    
    Change-Id: Icddd78a906ef7f9aed1891b824d0ef9dbe456b4c
    Signed-off-by: Jayanth Othayoth <ojayanth@gmail.com>

---
+++ plugin/plugin_registry.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "plugin/plugin_factory.hpp"
+#include "plugin/plugin_type.hpp"
+
+#include <memory>
+#include <unordered_map>
+
+namespace phosphor::logging
+{
+
+/**
+ * @brief Registry of supported plugin types.
+ *
+ * Maintains the mapping between plugin types and their
+ * associated plugin factories.
+ */
+class PluginRegistry
+{
+  public:
+    PluginRegistry() = default;
+    PluginRegistry(const PluginRegistry&) = delete;
+    PluginRegistry(PluginRegistry&&) = delete;
+    PluginRegistry& operator=(const PluginRegistry&) = delete;
+    PluginRegistry& operator=(PluginRegistry&&) = delete;
+    ~PluginRegistry() = default;
+
+    /**
+     * @brief Register a plugin factory.
+     *
+     * @param[in] type Plugin type.
+     * @param[in] factory Plugin factory.
+     */
+    void registerPlugin(plugin::Type type,
+                        std::unique_ptr<PluginFactory> factory);
+
+    /**
+     * @brief Lookup a plugin factory.
+     *
+     * @param[in] type Plugin type.
+     *
+     * @return Registered plugin factory or nullptr if
+     *         the plugin type is not registered.
+     */
+    const PluginFactory* lookup(plugin::Type type) const;
+
+  private:
+    /** Registered plugin factories. */
+    std::unordered_map<plugin::Type, std::unique_ptr<PluginFactory>> registry;
+};
+
+} // namespace phosphor::logging
