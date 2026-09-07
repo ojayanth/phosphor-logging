d47d152903397525e6e3cc8879b01f657ffa09f0
Jayanth Othayoth <ojayanth@gmail.com>
2026-09-07 06:50:53 -0500

    amd-event-log : Initial commit
    Change-Id: I2164934279ba219ebbe585a5c24d9ab7145e0601
    Signed-off-by: Jayanth Othayoth <ojayanth@gmail.com>

---
+++ extensions/amd-event-log/src/ael_info.cpp
@@ -0,0 +1,26 @@
+#include "ael_info.hpp"
+
+namespace phosphor::logging::extensions::ael
+{
+
+AELInfoProvider::AELInfoProvider(
+    const std::string& msg, Entry::Level level,
+    const std::map<std::string, std::string>& additionalData) :
+    msg(msg), level(level), additionalData(additionalData)
+{}
+
+AELInfo AELInfoProvider::get() const
+{
+    (void)level;
+
+    AELInfo info;
+
+    auto afidInfo = lookupAFID(msg, additionalData);
+
+    info.afid = afidInfo.afid;
+    info.fruList = afidInfo.frus;
+
+    return info;
+}
+
+} // namespace phosphor::logging::extensions::ael
